8ae00c2a2a8a1b8713b0b36c40c16cfe21938de0
unknown <lbvbxlbvbx@gmail.com>
2021-08-16 19:18:11 +0300

    not working solution

--- src/src/Solution.java
+++ src/src/Solution.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
 
 public class Solution {
+    //Given a string s, find the length of the longest substring without repeating characters.
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int k = 0;
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (set.add(s.charAt(i))) {
+                j++;
+            } else {
+                if (j > k) k = j;
+                j = 1;
+                set.clear();
+                set.add(s.charAt(i));
+            }
+
+        }
+        return k;
+    }
 }
